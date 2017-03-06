0ae59c1b52cae4bad1e14c7b209ced39c0807e9c
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2017-03-06 13:53:53 -0600

    update license headers

--- src/vol_err.h
+++ src/vol_err.h
@@ -1,6 +1,6 @@
 /* Dynvol - Dynamix Volume Toolkit
- * Copyright (C) 2014 Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
- * Copyright (C) 2014 OFTN Inc.
+ * Copyright © 2014-2017 Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
+ * Copyright © 2014-2017 ΩF:∅ OSWG
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
